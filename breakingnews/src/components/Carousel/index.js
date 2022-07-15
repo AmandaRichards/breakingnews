1d6bc788c708b94e67ffe7db20dea11829dbfe7a
Israel <93683580+blueishegg@users.noreply.github.com>
2022-07-15 14:28:17 +0100

    carousel to display 3 news items

--- breakingnews/src/components/Carousel/index.js
+++ breakingnews/src/components/Carousel/index.js
@@ -1,18 +1,58 @@
-import React from 'react';
+import React from "react";
 // import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import NewsItem from '../NewsItem';
+import { Carousel } from "react-responsive-carousel";
+import NewsItem from "../NewsItem";
+import css from "./index.module.css";
 
-const NewsCarousel = ({newsdata}) =>{
-    
-        return (
-            <Carousel>
-                {newsdata? newsdata.map(news => {return <NewsItem title={news.title} url={news.image_url} description={news.description} link={news.link} />}): 'news loading'}
-            </Carousel>
-        );
-    }
-
+const NewsCarousel = ({ newsdata }) => {
+  return (
+    <Carousel>
+      <div className={css.carousel}>
+        {newsdata
+          ? newsdata.slice(1, 4).map((news) => {
+              return (
+                <NewsItem
+                  title={news.title}
+                  url={news.image_url}
+                  description={news.description}
+                  link={news.link}
+                />
+              );
+            })
+          : "news loading"}
+      </div>
+      <div className={css.carousel}>
+        {newsdata
+          ? newsdata.slice(4, 7).map((news) => {
+              return (
+                <NewsItem
+                  title={news.title}
+                  url={news.image_url}
+                  description={news.description}
+                  link={news.link}
+                />
+              );
+            })
+          : "news loading"}
+      </div>
+      <div className={css.carousel}>
+        {newsdata
+          ? newsdata.slice(7, 10).map((news) => {
+              return (
+                <NewsItem
+                  title={news.title}
+                  url={news.image_url}
+                  description={news.description}
+                  link={news.link}
+                />
+              );
+            })
+          : "news loading"}
+      </div>
+    </Carousel>
+  );
+};
 
 export default NewsCarousel;
 
